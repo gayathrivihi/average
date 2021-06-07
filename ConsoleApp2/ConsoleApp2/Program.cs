6d1a0c307638cb4d31f2cf20f297a19b00da55ae
gayathri <n-gayathri.n-gayathri@capgemini.com>
2021-06-07 15:04:55 +0530

    avg

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+using System;
+using Util;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("");
+            System.Console.Read();
+
+            AddClass.Add();
+            MultiplyClass.Multiply();
+
+            string str = "Mohith";
+            String s = "Ram";
+
+
+
+           
+        }
+    }
+
+    struct gayathri
+    {
+
+    }
+}
